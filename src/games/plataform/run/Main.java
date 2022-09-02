009a5736e899740fee15a42810175ca427ce57dd
FelipeESchmidt <schmidtfe2@gmail.com>
2022-09-02 00:14:32 -0300

    instanciando janela principal do App na classe Main

--- src/games/plataform/run/Main.java
+++ src/games/plataform/run/Main.java
@@ -1,10 +1,13 @@
 package games.plataform.run;
 
+import games.plataform.gui.MainWindowForm;
+
 public class Main {
 
     public static void main(String[] args) {
         
-        
+        MainWindowForm window = new MainWindowForm();
+        window.setVisible(true);
         
     }
     
