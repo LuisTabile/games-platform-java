7a17dabc4a1c7021984bc08cfa06c34645946edd
Felipe Ernesto Schmidt <50564212+FelipeESchmidt@users.noreply.github.com>
2022-09-15 20:34:09 -0300

    fix: criando comentários para os métodos das classes dbModels (#2)

--- src/games/plataform/dbModels/Publishers.java
+++ src/games/plataform/dbModels/Publishers.java
@@ -2,6 +2,11 @@ package games.plataform.dbModels;
 
 public class Publishers {
     
+    /**
+     * Get the query string to get all publishers
+     *
+     * @return all publishers
+     */
     public static String getPublishers() {
         return "SELECT * FROM publisher";
     }
