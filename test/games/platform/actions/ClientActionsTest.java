3b292ff15d4ef98460483b0223c3b6eb8f227d0b
LUIS GUSTAVO TABILE <luis.tabile@unijui.edu.br>
2022-12-14 21:41:20 -0300

    tests: new tests

--- test/games/platform/actions/ClientActionsTest.java
+++ test/games/platform/actions/ClientActionsTest.java
@@ -4,10 +4,14 @@
  */
 package games.platform.actions;
 
+import games.platform.connection.DataBase;
 import games.platform.models.Client;
 import games.platform.models.Deposit;
 import games.platform.models.Game;
+import games.platform.utils.DbGlobal;
+import games.platform.utils.LoggerGlobal;
 import java.sql.Connection;
+import java.util.Date;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -20,47 +24,63 @@ import static org.junit.Assert.*;
  * @author luis.tabile
  */
 public class ClientActionsTest {
-    
+
+    private static Connection connection;
+
     public ClientActionsTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
+        DbGlobal.generateDb();
+        ClientActionsTest.connection = new DataBase().getConnection();
+        LoggerGlobal.generateLogger();
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
+
     }
-    
+
     @After
     public void tearDown() {
     }
 
     @Test
     public void testBuyOneGame() {
-        System.out.println("buyOneGame");
-        Client client = null;
-        Game game = null;
-        Connection dbConnection = null;
-        String expResult = "";
+        System.out.println("Teste 1 Comprar Jogos");
+        Client client = new Client(3, "Luis", 74512f);
+        Game game = new Game(1, "Dota", "MMORPG ", new Date(), 74f, 4, "Valve");
+        Connection dbConnection = ClientActionsTest.connection;
+        String expResult = "Compra Finalizada";
         String result = ClientActions.buyOneGame(client, game, dbConnection);
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
+     @Test
+    public void testBuyOneGame2() {
+        System.out.println("Teste 2 Comprar Jogos");
+        Client client = new Client(7, "Pedro", 77452f);
+        Game game = new Game(3, "", " ", new Date(), 34f, 7, "");
+        Connection dbConnection = ClientActionsTest.connection;
+        String expResult = "Compra Finalizada";
+        String result = ClientActions.buyOneGame(client, game, dbConnection);
+        assertEquals(expResult, result);
+    }
+
+
 
     @Test
     public void testAddCash() {
-        System.out.println("addCash");
-        Deposit deposit = null;
-        Connection dbConnection = null;
-        String expResult = "";
+        System.out.println("Teste 1 Adicionar Crédito");
+        Deposit deposit = new Deposit (1,1000);
+        Connection dbConnection = ClientActionsTest.connection;
+        String expResult = "Crédito adicionado";
         String result = ClientActions.addCash(deposit, dbConnection);
         assertEquals(expResult, result);
-        fail("The test case is a prototype.");
     }
-    
+
 }
